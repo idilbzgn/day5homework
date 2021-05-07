0a17bcab546247da9351f76590f72eff3be569d0
idilbzgn <82970481+idilbzgn@users.noreply.github.com>
2021-05-07 22:40:59 +0300

    Revert "Add files via upload"

--- business/abstracts/UserService.java
+++
@@ -1,7 +0,0 @@
-package business.abstracts;
-import dataAccess.abstracts.UserDao;
-import entities.concretes.User;
-public interface UserService {
-	void login(UserDao user);
-	void register(UserDao user);
-}
